17a7302daa116c8e1d010831a8150caee30b57c1
christina bartholomæussen <c.m.bartholo@gmail.com>
2022-04-25 15:35:14 +0200

    added files to gitignore

--- Organizr.Infrastructure/Models/Group.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Organizr.Infrastructure.Models;
-
-public class Group
-{
-    public int GroupId { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public List<Person> Persons { get; set; } = new ();
-}
--- Organizr.Infrastructure/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Organizr.Infrastructure.Models;
-
-public class Person
-{
-    public int PersonId { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public List<Group> Groups { get; set; } = new ();
-}
