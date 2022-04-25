72a183f3bdda60471fc7611270c13550eda7c788
christina bartholomæussen <c.m.bartholo@gmail.com>
2022-04-25 15:37:14 +0200

    lost files

---
+++ Organizr.Infrastructure/Models/Group.cs
@@ -0,0 +1,8 @@
+﻿namespace Organizr.Infrastructure.Models;
+
+public class Group
+{
+    public int GroupId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public List<Person> Persons { get; set; } = new ();
+}
