300b9d8ec43b7e6046fd96c16b7c878ee6f8be8a
Rune Petersen <runembpetersen@gmail.com>
2022-04-28 08:10:24 +0200

    Restart af Organizr.Admin appen, der var skadet efter flytning til Solution folder

--- Organizr.Admin/Program.cs
+++ Organizr.Admin/Program.cs
@@ -1,7 +1,10 @@
+using Organizr.Admin.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
@@ -16,4 +19,5 @@ app.UseStaticFiles();
 app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
 app.Run();
