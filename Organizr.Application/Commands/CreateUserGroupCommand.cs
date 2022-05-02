b17ad692255a4f2168eb76fa30d7d330e45de4c7
Rune Petersen <runembpetersen@gmail.com>
2022-05-02 12:50:33 +0200

    Implementeret "Opret gruppe" på Admin klienten.
    Tilføjet UserGroup til Entities, samt lavet migrationen så det er også er på databasen

---
+++ Organizr.Application/Commands/CreateUserGroupCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Organizr.Application.Responses;
+
+namespace Organizr.Application.Commands;
+
+public class CreateUserGroupCommand : IRequest<CreateUserGroupResponse>
+{
+    public string GroupName { get; init; } = string.Empty;
+    public bool GroupOpenForAll { get; init; }
+}
---
+++ Organizr.Application/Responses/CreateUserGroupResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Organizr.Application.Responses;
+
+public class CreateUserGroupResponse
+{
+    public string GroupName { get; set; } = string.Empty;
+    public bool Succeeded { get; set; }
+}
