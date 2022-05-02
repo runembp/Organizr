744c280429991fe94dd7fa0ec165e9827687a353
Rune Petersen <runembpetersen@gmail.com>
2022-05-02 08:23:20 +0200

    Småjusteringer

--- Organizr.Api/Controllers/AuthController.cs
+++ Organizr.Api/Controllers/AuthController.cs
@@ -3,30 +3,29 @@ using Microsoft.AspNetCore.Mvc;
 using Organizr.Application.Queries;
 using Organizr.Application.Responses;
 
-namespace Organizr.Api.Controllers
+namespace Organizr.Api.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
 {
-    [ApiController]
-    [Route("api/auth")]
-    public class AuthController : ControllerBase
+    private readonly IMediator _mediator;
+
+    public AuthController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public AuthController(IMediator mediator)
+    [HttpPost("login")]
+    public async Task<ActionResult<UserLoginResponse>> Login([FromBody] UserLoginRequest query)
+    {
+        var result = await _mediator.Send(query);
+
+        if (!result.Succeeded)
         {
-            _mediator = mediator;
+            return BadRequest("Failed login");
         }
 
-        [HttpPost("login")]
-        public async Task<ActionResult<UserLoginResponse>> Login([FromBody] UserLoginRequest query)
-        {
-            var result = await _mediator.Send(query);
-
-            if (!result.Succeeded)
-            {
-                return BadRequest("Failed login");
-            }
-
-            return Ok(result);
-        }
+        return Ok(result);
     }
 }
