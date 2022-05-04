d99c82aeb92d7f94773e99bac3c7f587ce4a9bef
christina bartholomæussen <c.m.bartholo@gmail.com>
2022-05-04 12:11:32 +0200

    Implmenteret plads til about us kan blive loaded ind til section på siden

--- Organizr.Member/member-application/src/app/about-us/about-us.component.ts
+++ Organizr.Member/member-application/src/app/about-us/about-us.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
 
+  aboutUsContent: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.aboutUsContent = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Bla bla blyant</h1>
+    </body>
+    </html>`;
   }
 
 }
