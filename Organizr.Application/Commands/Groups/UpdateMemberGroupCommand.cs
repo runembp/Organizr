d53462f97e5df18fddca4adfdb94649bf64a2fcb
Rune Petersen <runembpetersen@gmail.com>
2022-05-11 11:25:25 +0200

    Implementeret Rediger Gruppe informationer i Admin-klienten

---
+++ Organizr.Application/Commands/Groups/UpdateMemberGroupCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Organizr.Domain.Entities;
+
+namespace Organizr.Application.Commands.Groups;
+
+public class UpdateMemberGroupCommand : IRequest<MemberGroup>
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public bool IsOpen { get; init; }
+}
