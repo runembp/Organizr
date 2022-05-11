5baa765879253e270c158c9e9cb22eea6e08ab44
Rune Petersen <runembpetersen@gmail.com>
2022-05-11 14:16:28 +0200

    Implementeret Slet person fra Gruppe, i Overblik over Medlemmer-skærmbilledet i Admin-klienten

--- Organizr.Application/Common/Interfaces/IMemberGroupRepository.cs
+++ Organizr.Application/Common/Interfaces/IMemberGroupRepository.cs
@@ -7,5 +7,6 @@ public interface IMemberGroupRepository : IRepository<MemberGroup>
     public Task<MemberGroup?> GetMemberGroupWithMembers(int groupId);
     public Task<bool> GroupExists(string groupName);
     Task<MemberGroup> AddMemberToGroup(int groupId, int memberId);
+    Task<Member> RemoveMemberFromGroup(int groupId, int memberId);
     Task<MemberGroup> UpdateMemberGroup(MemberGroup memberGroup);
 }
