e62ca2e6a3575ce3b402f6499e8bbdf56005950b
christina bartholomæussen <c.m.bartholo@gmail.com>
2022-05-12 11:11:44 +0200

    kontaktformular

--- Organizr.Member/member-application/src/app/components/contact-us/contact-us-form/contact-us-form.component.ts
+++ Organizr.Member/member-application/src/app/components/contact-us/contact-us-form/contact-us-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us-form',
@@ -9,7 +10,20 @@ export class ContactUsFormComponent implements OnInit {
 
   constructor() { }
 
+  contactUsForm: FormGroup;
+
   ngOnInit(): void {
+    this.contactUsForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      phoneNumber: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      subject: new FormControl('', Validators.required),
+      message: new FormControl('', Validators.required)
+    });
   }
 
+  sendContactForm(): void {
+
+  }
 }
