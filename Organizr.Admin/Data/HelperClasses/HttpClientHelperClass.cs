ab9222fe8289a1afa1234d12a986bdaeb717d617
christina bartholomæussen <c.m.bartholo@gmail.com>
2022-05-17 21:06:24 +0200

    solved error

--- Organizr.Admin/Data/HelperClasses/HttpClientHelperClass.cs
+++ Organizr.Admin/Data/HelperClasses/HttpClientHelperClass.cs
@@ -7,9 +7,11 @@ namespace Organizr.Admin.Data.HelperClasses;
 
 public static class HttpClientHelperClass
 {
-   
     public static Task<HttpResponseMessage> DeleteAsJsonAsync(this HttpClient httpClient, string requestUri)
-        => httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Delete, requestUri));
+       => httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Delete, requestUri));
+
+    public static Task<HttpResponseMessage> DeleteAsJsonAsync<T>(this HttpClient httpClient, string requestUri, T data)
+        => httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Delete, requestUri) { Content = Serialize(data) });
 
     public static Task<HttpResponseMessage> PatchAsJsonAsync(this HttpClient httpClient, string requestUri)
         => httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Patch, requestUri));
