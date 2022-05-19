4ee1dc18d3d954677f663f05103245c8bf85023e
Rune Petersen <runembpetersen@gmail.com>
2022-05-19 10:05:36 +0200

    Implementation af GetMembershipsForMember endpoint, samt tilhørende kode igennem lagene

--- Organizr.Application/Common/Interfaces/IMembershipRepository.cs
+++ Organizr.Application/Common/Interfaces/IMembershipRepository.cs
@@ -5,4 +5,5 @@ namespace Organizr.Application.Common.Interfaces;
 public interface IMembershipRepository : IRepository<Membership>
 {
     Task<Membership?> CreateNewMembership(int groupId, int memberId, int roleId);
+    Task<List<Membership>?> GetMembershipsForMember(int memberId);
 }
---
+++ Organizr.Application/Requests/Memberships/GetMembershipsForMemberRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Organizr.Domain.Entities;
+
+namespace Organizr.Application.Requests.Memberships;
+
+public class GetMembershipsForMemberRequest : IRequest<List<Membership>?>
+{
+    public int MemberId { get; init; }
+}
