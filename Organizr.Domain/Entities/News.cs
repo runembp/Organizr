e74670a347b3069b30688c5cf420089d9b9e52e0
christina bartholomæussen <c.m.bartholo@gmail.com>
2022-05-19 11:11:40 +0200

    news entity

---
+++ Organizr.Domain/Entities/News.cs
@@ -0,0 +1,12 @@
+﻿namespace Organizr.Domain.Entities;
+
+public class News
+{
+    public int Id { get; set; }
+    public string Titel { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateOnly CreatedAtDate { get; set; }
+    public TimeOnly CreatedAtTime { get; set; }
+    public Boolean IsPublic { get; set; }
+}
+
