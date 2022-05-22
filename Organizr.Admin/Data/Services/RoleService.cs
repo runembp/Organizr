4e250d531d0e596cb5d2398c80e541f3c1edb0c4
Rune Petersen <runembpetersen@gmail.com>
2022-05-22 11:37:25 +0200

    Opdateret MemberInfo skærmbilledet til at gøre brug af Cards, samt tilføjet OrganisationsAdministrator checkbox

---
+++ Organizr.Admin/Data/Services/RoleService.cs
@@ -0,0 +1,24 @@
+﻿using Organizr.Admin.Data.HelperClasses;
+
+namespace Organizr.Admin.Data.Services;
+
+public class RoleService
+{
+    private readonly HttpClient _httpClient;
+
+    public RoleService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<bool> IsUserOrganizationAdministrator(int memberId)
+    {
+        return await _httpClient.GetFromJsonAsync<bool>($"api/roles/{memberId}");
+    }
+
+    public async Task<bool> UpdateMemberRole(int memberId, bool setUserOrganizationAdministrator)
+    {
+        var result = await _httpClient.PatchAsJsonAsync("api/roles/{memberId}", setUserOrganizationAdministrator);
+        return result.IsSuccessStatusCode;
+    }
+}
