7738a6845c1e3b8eb12f79101e26e537795f8d9e
christina bartholomæussen <c.m.bartholo@gmail.com>
2022-05-23 10:25:48 +0200

    Initial create af databasen

--- Organizr.Infrastructure/Persistence/OrganizrDbContext.cs
+++ Organizr.Infrastructure/Persistence/OrganizrDbContext.cs
@@ -31,12 +31,9 @@ namespace Organizr.Infrastructure.Persistence
             modelBuilder.Entity<NewsPost>(entity =>
             {
                 entity.Property(member => member.Id).IsRequired();
+                entity.Property(memberGroup => memberGroup.Id).IsRequired();
             });
 
-            modelBuilder.Entity<NewsPost>()
-                .HasOne(n => n.MemberGroup)
-                .WithMany(mp =>mp.NewsPosts);
-            
         }
     }
 }
