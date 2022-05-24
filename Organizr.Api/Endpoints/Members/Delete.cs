51212dd03e43628f9a3b4e9358c2dca10f875e24
Rune Petersen <runembpetersen@gmail.com>
2022-05-24 11:29:07 +0200

    Implementeret ApiEndpoints for Member og udfaset MemberController

---
+++ Organizr.Api/Endpoints/Members/Delete.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Organizr.Application.Commands.Members;
+using Organizr.Application.Responses.Member;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Organizr.Api.Endpoints.Members;
+
+public class Delete : BaseApiEndpoint
+{
+    public Delete(IMediator mediator) : base(mediator)
+    {
+    }
+    
+    [HttpDelete("api/members/{memberId:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DeleteMemberResponse))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [SwaggerOperation(
+        Summary = "Deletes a Member",
+        Tags = new [] {"MemberEndpoint"})]
+    public async Task<IActionResult> DeleteMemberById([FromRoute] int memberId)
+    {
+        if (memberId <= 0)
+        {
+            return BadRequest("Medlems id er ikke i et korrekt format");
+        }
+
+        var result = await Mediator.Send(new DeleteMemberCommand {MemberId = memberId});
+
+        if (!result.Succeeded)
+        {
+            return BadRequest(result);
+        }
+
+        return Ok(result);
+    }
+}
---
+++ Organizr.Api/Endpoints/Members/GetAll.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Organizr.Application.Requests.Groups;
+using Organizr.Domain.Entities;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Organizr.Api.Endpoints.Members;
+
+public class GetAll : BaseApiEndpoint
+{
+    public GetAll(IMediator mediator) : base(mediator)
+    {
+    }
+    
+    [HttpGet("api/members")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Member>))]
+    [SwaggerOperation(
+        Summary = "Gets a list of all members",
+        Tags = new [] {"MemberEndpoint"})]
+    public async Task<ActionResult<List<Member>>> Handle(CancellationToken cancellationToken = default)
+    {
+        var result = await Mediator.Send(new GetAllMembersRequest(), cancellationToken);
+        return Ok(result);
+    }
+    
+}
