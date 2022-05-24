4a6c5462c213d8d697dcb188c0fe920b3cb84d42
Rune Petersen <runembpetersen@gmail.com>
2022-05-24 11:55:35 +0200

    Udfaset Login, NewsPost, Configuration og RoleController til endpoint-pattern

---
+++ Organizr.Api/Endpoints/Configurations/GetByType.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Organizr.Application.Requests.Configurations;
+using Organizr.Domain.Entities;
+using Organizr.Domain.Enums;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Organizr.Api.Endpoints.Configurations;
+
+public class GetByType : BaseApiEndpoint
+{
+    public GetByType(IMediator mediator) : base(mediator)
+    {
+    }
+    
+    [HttpGet("api/configurations/type/{configType:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Configuration>))]
+    [SwaggerOperation(
+        Summary = "Gets a list of Configurations based on type",
+        Tags = new [] {"ConfigurationEndpoint"})]
+    public async Task<IActionResult> Handle([FromRoute] int configType)
+    {
+        var result = await Mediator.Send(new GetAllConfigurationsOfTypeRequest {ConfigType = (ConfigType) configType});
+        return Ok(result);
+    }
+}
---
+++ Organizr.Api/Endpoints/Newsposts/GetAll.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Organizr.Application.Requests.News;
+using Organizr.Domain.Entities;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Organizr.Api.Endpoints.Newsposts;
+
+public class GetAll : BaseApiEndpoint
+{
+    public GetAll(IMediator mediator) : base(mediator)
+    {
+    }
+    
+    [HttpGet("api/newsposts")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<NewsPost>))]
+    [SwaggerOperation(
+        Summary = "Gets a list of all news posts",
+        Tags = new [] {"NewsPostEndpoint"})]
+    public async Task<IActionResult> Handle()
+    {
+        var result = await Mediator.Send(new GetAllPublicNewsPostsRequest());
+        return Ok(result);
+    }
+}
