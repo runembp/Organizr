d8ee16024ca47f21ecae91a6adbdb86fa1a1c7e5
christina bartholomæussen <c.m.bartholo@gmail.com>
2022-05-25 11:41:20 +0200

    resolve comments

--- Organizr.Application/Requests/News/GetAllNewsPostsByGroupIdRequest.cs
+++ Organizr.Application/Requests/News/GetAllNewsPostsByGroupIdRequest.cs
@@ -3,7 +3,7 @@ using Organizr.Domain.Entities;
 
 namespace Organizr.Application.Requests.News;
 
-public class GetAllNewsPostsByGroupIdRequest : IRequest<List<NewsPost>>
+public class GetAllNewsPostsByGroupIdRequest : IRequest<List<NewsPost>?>
 {
     public int GroupId { get; set; }
 }
